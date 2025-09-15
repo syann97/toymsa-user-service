958a6e0038d2db9ede086b1356fd23d6d67e72c0
syann97 <shallom75321@gmail.com>
2025-09-15 19:23:53 +0900

    feat: Set up and configure the Eureka Client project #1

---
+++ src/test/java/com/github/syann97/toymsa/user/DiscoveryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.syann97.toymsa.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
